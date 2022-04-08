409aa3e0336353b9af7c004b723f245fa0af609d
Mayank Chouksey <97603473+Mchouksey2711@users.noreply.github.com>
2022-04-09 01:42:48 +0530

    Time: 4 ms (86.13%), Space: 7.9 MB (74.29%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+       /*countBits[0]=0;    
+        for(int i=1;i<=n;i++)
+        {
+            int count=0;
+            while(i)
+            {
+                i=i&(i-1);
+                count++;
+            }
+            countBits[i]=count;
+        }
+        return countBits;
+        */
+         vector<int>res(n+1);
+        res[0] = 0;
+        for(int i = 1 ; i <= n ; i++)
+            res[i] = res[i/2] + (i&1);
+        return res;
+    }
+};
