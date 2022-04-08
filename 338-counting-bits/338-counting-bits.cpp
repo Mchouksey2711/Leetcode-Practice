aac09cbc3d16c9471f5347a45162a462bc0c5c31
Mayank Chouksey <97603473+Mchouksey2711@users.noreply.github.com>
2022-04-09 01:46:47 +0530

    Time: 4 ms (86.13%), Space: 7.8 MB (74.29%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -14,7 +14,7 @@ public:
         }
         return countBits;
         */
-         vector<int>res(n+1);
+        vector<int>res(n+1);
         res[0] = 0;
         for(int i = 1 ; i <= n ; i++)
             res[i] = res[i/2] + (i&1);
