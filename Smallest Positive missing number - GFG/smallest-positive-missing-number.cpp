9d3a5fba68218950a2abda7b63eb30aabc392afb
Mayank Chouksey <97603473+Mchouksey2711@users.noreply.github.com>
2022-04-29 17:40:33 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -12,13 +12,32 @@ class Solution
     //Function to find the smallest positive number missing from the array.
     int missingNumber(int arr[], int n) 
     { 
-        sort(arr,arr+n);
-        int min=1;
-        for(int i=0;i<n;i++){
-            if(arr[i]==min)
-               min++;
-        }
-        return min;
+        // sort(arr,arr+n);
+        // int min=1;
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==min)
+        //       min++;
+        // }
+        // return min;
+        
+        const int N=1e6 +2;
+        bool check[N];
+        for(int i=0;i<N;i++)
+           check[i]= false;
+           
+         for(int i=0;i<n;i++){
+             if(arr[i]>=0)
+                check[arr[i]]= true;
+         }
+         
+         int ans=-1;
+         for(int i=1;i<N;i++){
+             if(check[i] == false){
+                ans=i;
+                break;
+             }
+         }
+         return ans;
     } 
 };
 
