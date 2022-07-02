3b62632fb8006ede734a06b4cb29a622e20e7b2e
Mayank Chouksey <97603473+Mchouksey2711@users.noreply.github.com>
2022-07-03 01:32:50 +0530

    Time: 35 ms (46.20%), Space: 23.3 MB (5.06%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,7 +3,12 @@ public:
     
     void reverseString(vector<char>& s) 
     {
-         reverse(s.begin(),s.end());
-        
+         int start=0;
+         int end=s.size()-1;
+         while(start<=end){
+             swap(s[start], s[end]);
+             start++;
+             end--;
+         }
     }
 };
