f91e4d72a0342d7f9400f59c30314e9e77bd6199
Mayank Chouksey <97603473+Mchouksey2711@users.noreply.github.com>
2022-08-11 18:00:04 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (49.74%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector <int> ans;
+        int size= arr.size();
+        
+        sort(arr.begin(), arr.end());
+        
+        for(int i=0; i<size;){
+            int count= 1;
+            for(int j=i+1; j<size; j++){
+                if(arr[i] == arr[j])
+                    count++;
+                else
+                    break;
+            }
+            ans.push_back(count);
+            i=i+count;
+        }
+        sort(ans.begin(), ans.end());
+        for(int i=0; i<ans.size()-1; i++){
+            if(ans[i] == ans[i+1])
+                return false;
+        }
+        return true;
+    }
+};
